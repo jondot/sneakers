a7fc0185cb467af26a2d8c51ee3a4da4cdd87e25
Sebastian Edwards <me@sebastianedwards.co.nz>
2014-01-08 11:29:41 +1300

    Allow binding to multiple routing keys.

--- lib/sneakers/queue.rb
+++ lib/sneakers/queue.rb
@@ -29,9 +29,13 @@ class Sneakers::Queue
     handler = @handler_klass.new(@channel)
 
     routing_key = @opts[:routing_key] || @name
+    routing_keys = routing_key.respond_to?(:each) ? routing_key : [routing_key]
 
     queue = @channel.queue(@name, :durable => @opts[:durable])
-    queue.bind(@exchange, :routing_key => routing_key)
+
+    routing_keys.each do |key|
+      queue.bind(@exchange, :routing_key => key)
+    end
 
     @consumer = queue.subscribe(:block => false, :ack => @opts[:ack]) do | hdr, props, msg | 
       worker.do_work(hdr, props, msg, handler)
