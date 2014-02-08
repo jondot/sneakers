e0560a71f0143bde5f005030f4c84e6945546d5c
jondot <jondotan@gmail.com>
2014-02-08 17:01:48 +0200

    dust off metrics and profiling workers. locking bunny on 1.1.x (safer?)

--- examples/profiling_worker.rb
+++ examples/profiling_worker.rb
@@ -1,25 +1,39 @@
 $: << File.expand_path('../lib', File.dirname(__FILE__))
 require 'sneakers'
 require 'sneakers/runner'
-require 'ruby-prof'
+require 'logger'
 
+
+profiling = ARGV[0]
+messages = 100_000
+
+
+if profiling
+  require 'ruby-prof'
+  messages /= 100 # profiling makes everything much slower (around 300req/s)
+end
+
+Sneakers.configure
+Sneakers.logger.level = Logger::ERROR
+
+Sneakers::Worker.configure_logger(Logger.new('/dev/null'))
 
 puts "feeding messages in"
-1000.times {
+messages.times {
   Sneakers.publish("{}", :to_queue => 'downloads')
+}
 puts "done"
 
 
 class ProfilingWorker
   include Sneakers::Worker
   from_queue 'downloads',
-             :env => '',
-             :durable => false,
+             :env => nil,
              :ack => true,
              :threads => 50,
              :prefetch => 50,
              :timeout_job_after => 1,
-             :exchange => 'dummy',
+             :exchange => 'sneakers',
              :heartbeat => 5
   def work(msg)
     ack!
@@ -28,28 +42,29 @@ end
 
 
 
-r = Sneakers::Runner.new
-Sneakers::Worker.configure_logger(Logger.new('/dev/null'))
+r = Sneakers::Runner.new([ProfilingWorker])
 
 # ctrl-c and Ruby 2.0 breaks signal handling
 # Sidekiq has same issues
 # https://github.com/mperham/sidekiq/issues/728
 #
 # so we use a timeout and a thread that kills profiling
-puts "profiling start"
-RubyProf.start
+if profiling
+  puts "profiling start"
+  RubyProf.start
 
-Thread.new do
-  sleep 10
-  puts "stopping profiler"
-  result = RubyProf.stop
 
-  # Print a flat profile to text
-  printer = RubyProf::FlatPrinter.new(result)
-  printer.print(STDOUT)
-  r.stop
-  exit(0)
+  Thread.new do
+    sleep 10
+    puts "stopping profiler"
+    result = RubyProf.stop
+
+    # Print a flat profile to text
+    printer = RubyProf::FlatPrinter.new(result)
+    printer.print(STDOUT)
+    exit(0)
+  end
 end
 
-r.run([ ProfilingWorker ])
+r.run
 
