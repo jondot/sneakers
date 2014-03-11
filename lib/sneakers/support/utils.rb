c1ee5cb27c233bbdd0c682700e6aec35b6a23a94
kellydunn <defaultstring@gmail.com>
2014-03-11 15:11:14 -0700

    Adding in a new class level method that will attempt to find a constant that is deeply nested in a module namespace.

--- lib/sneakers/support/utils.rb
+++ lib/sneakers/support/utils.rb
@@ -2,12 +2,13 @@ class Sneakers::Utils
   def self.make_worker_id(namespace)
     "worker-#{namespace}:#{'1'}:#{rand(36**6).floor.to_s(36)}"  # jid, worker id. include date.
   end
+
   def self.parse_workers(workerstring)
     missing_workers = []
     workers = (workerstring || '').split(',').map do |k|
       begin
-        w = Object.const_get(k)
-      rescue
+        w = Sneakers::Utils.find_const_by_classname(k)
+      rescue 
         missing_workers << k
       end
       w
@@ -15,4 +16,28 @@ class Sneakers::Utils
 
     [workers, missing_workers]
   end
+
+  # Finds and returns the constant that is identified by the passed in klassname.
+  # If the klassname is deeply nested in multiple modules, it will iteratively
+  # attempt to retrieve each nested module until it reaches the end.
+  #
+  # @param klassname {String}.  A String representation of a class name, like "Foo" or "Foo::Bar::Baz"
+  # @return {Class}. A Class that is identified by the passed in klassname.
+  def self.find_const_by_classname(klassname)
+    current = Object
+    modules = klassname.split("::")
+
+    if modules.length > 1
+      modules.each do |m|
+        current = current.const_get(m)
+      end
+      
+      return current
+    else
+
+      return Object.const_get(klassname)
+    end
+    
+  end
+
 end
