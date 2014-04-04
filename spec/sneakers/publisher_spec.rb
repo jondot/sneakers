c331d118f5b2f4cdb5c5f7a5d06cadf8cbc5f40e
John O'Gara <jogara@paddypower.com>
2014-04-04 14:41:00 +0100

    Added persistence option to publisher, removed Gemfile.lock, linter some files

--- spec/sneakers/publisher_spec.rb
+++ spec/sneakers/publisher_spec.rb
@@ -1,53 +1,65 @@
 require 'spec_helper'
 require 'sneakers'
 
-
 describe Sneakers::Publisher do
-  describe "#publish" do
-    it "should publish a message to an exchange" do
+  overload_amqp
+  describe '#publish' do
+    it 'should publish a message to an exchange' do
       xchg = Object.new
-      mock(xchg).publish("test msg", :routing_key => "downloads")
+      mock(xchg).publish('test msg', routing_key: 'downloads')
 
       p = Sneakers::Publisher.new
-      p.exchange = xchg
+      p.instance_variable_set(:@exchange, xchg)
 
-      mock(p).ensure_connection!{}
-      p.publish("test msg", :to_queue => 'downloads')
+      mock(p).ensure_connection! {}
+      p.publish('test msg', to_queue: 'downloads')
     end
 
-    it "should not reconnect if already connected" do
+    it 'should publish with th persistence specified' do
       xchg = Object.new
-      mock(xchg).publish("test msg", :routing_key => "downloads")
+      mock(xchg).publish('test msg', routing_key: 'downloads', persistence: true)
 
       p = Sneakers::Publisher.new
-      p.exchange = xchg
-      mock(p).connected?{true}
+      p.instance_variable_set(:@exchange, xchg)
+
+      mock(p).ensure_connection! {}
+      p.publish('test msg', to_queue: 'downloads', persistence: true)
+    end
+
+    it 'should not reconnect if already connected' do
+      xchg = Object.new
+      mock(xchg).publish('test msg', routing_key: 'downloads')
+
+      p = Sneakers::Publisher.new
+      p.instance_variable_set(:@exchange, xchg)
+
+      mock(p).connected? { true }
       mock(p).ensure_connection!.times(0)
 
-      p.publish("test msg", :to_queue => 'downloads')
+      p.publish('test msg', to_queue: 'downloads')
     end
 
-    it "should connect to rabbitmq configured on Sneakers.configure" do
+    it 'should connect to rabbitmq configured on Sneakers.configure' do
       Sneakers.configure(
-        :amqp => "amqp://someuser:somepassword@somehost:5672",
-        :heartbeat => 1, :exchange => 'another_exchange',
-        :exchange_type => :topic,
-        :durable => false)
+        amqp: 'amqp://someuser:somepassword@somehost:5672',
+        heartbeat: 1, exchange: 'another_exchange',
+        exchange_type: :topic,
+        durable: false)
 
       channel = Object.new
-      mock(channel).exchange("another_exchange", :type => :topic, :durable => false) {
-        mock(Object.new).publish("test msg", :routing_key => "downloads")
-      }
+      mock(channel).exchange('another_exchange', type: :topic, durable: false) do
+        mock(Object.new).publish('test msg', routing_key: 'downloads')
+      end
 
       bunny = Object.new
       mock(bunny).start
       mock(bunny).create_channel { channel }
 
-      mock(Bunny).new("amqp://someuser:somepassword@somehost:5672", :heartbeat => 1 ) { bunny }
+      mock(Bunny).new('amqp://someuser:somepassword@somehost:5672', heartbeat: 1) { bunny }
 
       p = Sneakers::Publisher.new
 
-      p.publish("test msg", :to_queue => 'downloads')
+      p.publish('test msg', to_queue: 'downloads')
 
     end
   end
