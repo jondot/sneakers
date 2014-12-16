edb1f0bbec580d5a416e35c0be99e54f8f765386
Jonathan Calvert <jcalvert@taximagic.com>
2014-12-16 10:57:11 -0500

    Change Rake task to look for a configuration file over reading a worker list from an ENV. This allows the worker list to be changed by just deploying instead of having to alter the server ENV variables (which can be more involved if managed through something like chef). Also this file can effectively define 'groupings' so that it is not required to add a new managed process each time there's a desire to add a new worker that shouldn't share a process pool (IE long running batch jobs vs push notifications) - new processes are forked for each group which maintains a separate control process for each pool. Signals are passed to each child. This could possibly be rewritten much more effectively as a shell script

--- lib/sneakers/tasks.rb
+++ lib/sneakers/tasks.rb
@@ -6,17 +6,37 @@ task :environment
 namespace :sneakers do
   desc "Start work (set $WORKERS=Klass1,Klass2)"
   task :run  => :environment do
+    worker_group_config_file = ENV['WORKER_GROUP_CONFIG'] || "./config/sneaker_worker_groups.json" 
+    if File.exists?(worker_group_config_file)
+      @pids = []
+      worker_config = JSON.parse(File.read(worker_group_config_file))
+      worker_config["group_names"].each do |group_name| 
+        workers, missing_workers = Sneakers::Utils.parse_workers(worker_config[group_name]["classes"])
+        unless missing_workers.empty?
+          puts "Missing workers: #{missing_workers.join(', ')}" if missing_workers
+          puts "Did you `require` properly?"
+          exit(1)
+        end
+        @pids << fork do
+          @r=Sneakers::Runner.new(workers, {:workers => worker_config[group_name]["processes"]})
+          @r.run
+        end
+      end
+      ["TERM", "USR1", "HUP", "USR2"].each do |signal|
+        Signal.trap(signal){ @pids.each{|pid| Process.kill(signal, pid) } }
+      end
+      Process.waitall
+    else
+      workers, missing_workers = Sneakers::Utils.parse_workers(ENV['WORKERS'])
 
-    workers, missing_workers = Sneakers::Utils.parse_workers(ENV['WORKERS'])
+      unless missing_workers.empty?
+        puts "Missing workers: #{missing_workers.join(', ')}" if missing_workers
+        puts "Did you `require` properly?"
+        exit(1)
+      end
 
-    unless missing_workers.empty?
-      puts "Missing workers: #{missing_workers.join(', ')}" if missing_workers
-      puts "Did you `require` properly?"
-      exit(1)
-    end
-
-    if workers.empty?
-      puts <<EOF
+      if workers.empty?
+        puts <<EOF
 Error: No workers found.
 Please set the classes of the workers you want to run like so:
 
@@ -24,11 +44,12 @@ Please set the classes of the workers you want to run like so:
   $ rake sneakers:run
 
 EOF
-      exit(1)
+        exit(1)
+      end
+
+      r = Sneakers::Runner.new(workers)
+
+      r.run
     end
-
-    r = Sneakers::Runner.new(workers)
-
-    r.run
   end
 end
