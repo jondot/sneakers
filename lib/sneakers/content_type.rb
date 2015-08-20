f608e2ec16194ff9ee37e79b6ef8a6e98261f8c8
Ed Robinson <ed.robinson@reevoo.com>
2015-08-20 21:02:20 +0100

    Further tweaks for performance ...
    I was not really happy that trying to short circuit
    and save a few method calls when the feature is
    not in use only to impact on the feature itself.
    
    This makes the whole thing a little more efficient
    by switching to attr_reader and imposes a constant
    cost whichever execution path is followed.

--- lib/sneakers/content_type.rb
+++ lib/sneakers/content_type.rb
@@ -1,25 +1,25 @@
 module Sneakers
   class ContentType
     def self.register(content_type: nil, serializer: nil, deserializer: nil)
-      @_active = true
       @_types[content_type] = new(serializer, deserializer)
     end
 
     def self.serialize(payload, content_type)
-      return payload unless @_active
-      @_types[content_type].serialize(payload)
+      @_types[content_type].serializer.(payload)
     end
 
     def self.deserialize(payload, content_type)
-      return payload unless @_active
-      @_types[content_type].deserialize(payload)
+      @_types[content_type].deserializer.(payload)
     end
 
     def self.reset!
-      @_active = false
       @_types = Hash.new(
-        new(->(payload) { payload }, ->(payload) { payload })
+        new(passthrough, passthrough)
       )
+    end
+
+    def self.passthrough
+      ->(payload) { payload }
     end
 
     def initialize(serializer, deserializer)
@@ -27,13 +27,7 @@ module Sneakers
       @deserializer = deserializer
     end
 
-    def serialize(payload)
-      @serializer.(payload)
-    end
-
-    def deserialize(payload)
-      @deserializer.(payload)
-    end
+    attr_reader :serializer, :deserializer
 
     reset!
   end
--- spec/sneakers/content_type_spec.rb
+++ spec/sneakers/content_type_spec.rb
@@ -7,36 +7,36 @@ describe Sneakers::ContentType do
     Sneakers::ContentType.reset!
   end
 
-  describe '#serialize' do
-    it 'uses the given serializer' do
-      json = Sneakers::ContentType.new(
-        ->(payload) { JSON.dump(payload) },
-        nil,
-      )
-
-      json.serialize('foo' => 'bar').must_equal('{"foo":"bar"}')
-    end
-  end
-
-  describe '#deserialize' do
+  describe '.deserialize' do
     it 'uses the given deserializer' do
-      json = Sneakers::ContentType.new(
-        nil,
-        ->(payload) { JSON.parse(payload) },
+      Sneakers::ContentType.register(
+        content_type: 'application/json',
+        deserializer: ->(payload) { JSON.parse(payload) },
       )
 
-      json.deserialize('{"foo":"bar"}').must_equal('foo' => 'bar')
+      Sneakers::ContentType.deserialize('{"foo":"bar"}', 'application/json').must_equal('foo' => 'bar')
     end
   end
 
   describe '.serialize' do
-    it 'returns a pass through by default' do
+     it 'uses the given serializer' do
+      Sneakers::ContentType.register(
+        content_type: 'application/json',
+        serializer: ->(payload) { JSON.dump(payload) },
+      )
+
+      Sneakers::ContentType.serialize({ 'foo' => 'bar' }, 'application/json').must_equal('{"foo":"bar"}')
+    end
+
+    it 'passes the payload through by default' do
       payload = "just some text"
       Sneakers::ContentType.serialize(payload, 'unknown/type').must_equal(payload)
       Sneakers::ContentType.deserialize(payload, 'unknown/type').must_equal(payload)
+      Sneakers::ContentType.serialize(payload, nil).must_equal(payload)
+      Sneakers::ContentType.deserialize(payload, nil).must_equal(payload)
     end
 
-    it 'returns a pass through if not found type' do
+    it 'passes the payload through if type not found' do
       Sneakers::ContentType.register(content_type: 'found/type')
       payload = "just some text"
 
