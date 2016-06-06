79a0d529ec9cc88d78fbd3705116f395bea96c81
goromlagche <mrinmoy.das91@gmail.com>
2016-06-06 22:25:48 +0530

    [ISSUE-171] fix specs

--- lib/sneakers/concerns/logging.rb
+++ lib/sneakers/concerns/logging.rb
@@ -10,7 +10,7 @@ module Sneakers
 
       module ClassMethods
         def logger
-          @logger ||= Logger.new(STDOUT)
+          @logger ||= configure_logger
         end
 
         def logger=(logger)
@@ -25,6 +25,7 @@ module Sneakers
             @logger.level = Logger::INFO
             @logger.formatter = Sneakers::Support::ProductionFormatter
           end
+          @logger
         end
       end
     end
