1dc6265db58d93d47d9d19994c1bd244d895591b
gideonaa <gideonaa@gmail.com>
2016-06-19 17:07:31 -0700

    Ensure queue exists before publishing to avoid lost messages

--- lib/sneakers/publisher.rb
+++ lib/sneakers/publisher.rb
@@ -8,6 +8,7 @@ module Sneakers
     def publish(msg, options = {})
       @mutex.synchronize do
         ensure_connection! unless connected?
+        ensure_queue!(options[:to_queue])
       end
       to_queue = options.delete(:to_queue)
       options[:routing_key] ||= to_queue
@@ -16,7 +17,7 @@ module Sneakers
     end
 
 
-    attr_reader :exchange
+    attr_reader :exchange, :queue
 
   private
     def ensure_connection!
@@ -35,6 +36,14 @@ module Sneakers
 
     def create_bunny_connection
       Bunny.new(@opts[:amqp], :vhost => @opts[:vhost], :heartbeat => @opts[:heartbeat], :logger => Sneakers::logger)
+    end
+
+    def ensure_queue!(name)
+      # If the @queue attribute is already set, simply return
+      # otherwise declare+bind queue to ensure the msg is sent somewhere
+      return if @queue
+      @queue = @channel.queue(name, @opts[:queue_options])
+      @queue.bind(@exchange, routing_key: name)
     end
   end
 end
