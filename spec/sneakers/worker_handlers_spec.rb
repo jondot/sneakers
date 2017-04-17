e5a2e202268b7485c283936bc22caf0153c0b8c4
Dong Wook Koo <dongwook@culinaryagents.com>
2017-04-17 18:09:00 -0400

    Spec for Expbackoff.

--- spec/sneakers/worker_handlers_spec.rb
+++ spec/sneakers/worker_handlers_spec.rb
@@ -3,6 +3,7 @@ require 'sneakers'
 require 'timeout'
 require 'sneakers/handlers/oneshot'
 require 'sneakers/handlers/maxretry'
+require 'sneakers/handlers/expbackoff'
 require 'json'
 
 
@@ -388,6 +389,306 @@ describe 'Handlers' do
           data['num_attempts'].must_equal(2)
           data['payload'].must_equal(Base64.encode64(payload.to_json))
           data['properties'].must_equal(Base64.encode64(@props_with_x_death.to_json))
+        end
+
+      end
+
+    end
+  end
+
+  describe 'Expbackoff' do
+    let(:max_retries) { 3 }
+    let(:props_with_x_death_count) {
+      {
+        :headers => {
+          'count' => 3,
+          :backoff => 240,
+          "x-death" => [
+                        {
+                          "count" => 1,
+                          "reason" => "expired",
+                          "queue" => "downloads-backoff-240",
+                          "time" => Time.now,
+                          "exchange" => "downloads-backoff",
+                          "routing-keys" => ["downloads"]
+                        }
+                       ]
+        },
+        :delivery_mode => 1
+      }
+    }
+
+    before(:each) do
+      @opts = {
+        :exchange => 'sneakers',
+        :queue_options => {
+          :durable => 'true',
+        }
+      }.tap do |opts|
+        opts[:retry_max_times] = max_retries unless max_retries.nil?
+      end
+
+      mock(queue).name { 'downloads' }
+
+      @backoff_exchange = Object.new
+      @error_exchange = Object.new
+      @requeue_exchange = Object.new
+
+      @backoff_60_queue = Object.new
+      @backoff_120_queue = Object.new
+      @backoff_240_queue = Object.new
+      @error_queue = Object.new
+
+      mock(channel).exchange('downloads-backoff',
+                             :type => 'headers',
+                             :durable => 'true').once { @backoff_exchange }
+      mock(channel).exchange('downloads-error',
+                             :type => 'topic',
+                             :durable => 'true').once { @error_exchange }
+      mock(channel).exchange('downloads-retry-requeue',
+                             :type => 'topic',
+                             :durable => 'true').once { @requeue_exchange }
+
+      mock(channel).queue('downloads-backoff-60',
+                          :durable => 'true',
+                          :arguments => {
+                            :'x-dead-letter-exchange' => 'downloads-retry-requeue',
+                            :'x-message-ttl' => 60000
+                          }
+                          ).once { @backoff_60_queue }
+      mock(@backoff_60_queue).bind(@backoff_exchange, :arguments => {:backoff => 60})
+
+      mock(channel).queue('downloads-backoff-120',
+                          :durable => 'true',
+                          :arguments => {
+                            :'x-dead-letter-exchange' => 'downloads-retry-requeue',
+                            :'x-message-ttl' => 120000
+                          }
+                          ).once { @backoff_120_queue }
+      mock(@backoff_120_queue).bind(@backoff_exchange, :arguments => {:backoff => 120})
+
+      mock(channel).queue('downloads-backoff-240',
+                          :durable => 'true',
+                          :arguments => {
+                            :'x-dead-letter-exchange' => 'downloads-retry-requeue',
+                            :'x-message-ttl' => 240000
+                          }
+                          ).once { @backoff_240_queue }
+      mock(@backoff_240_queue).bind(@backoff_exchange, :arguments => {:backoff => 240})
+
+      mock(channel).queue('downloads-error',
+                          :durable => 'true').once { @error_queue }
+      mock(@error_queue).bind(@error_exchange, :routing_key => '#')
+
+      mock(queue).bind(@requeue_exchange, :routing_key => '#')
+
+      @handler = Sneakers::Handlers::Expbackoff.new(channel, queue, @opts)
+
+      @header = Object.new
+      stub(@header).delivery_tag { 37 }
+
+      @props = {}
+      @props_with_x_death = {
+        :headers => {
+          'count' => 1,
+          :backoff => 60,
+          "x-death" => [
+                        {
+                          "count" => 1,
+                          "reason" => "expired",
+                          "queue" => "downloads-backoff-60",
+                          "time" => Time.now,
+                          "exchange" => "downloads-backoff",
+                          "routing-keys" => ["downloads"]
+                        }
+                       ]
+        },
+        :delivery_mode => 1}
+    end
+
+    # it 'allows overriding the retry exchange name'
+    # it 'allows overriding the error exchange name'
+    # it 'allows overriding the retry timeout'
+
+    describe '#do_work' do
+      before do
+        @now = Time.now
+      end
+
+      # Used to stub out the publish method args. Sadly RR doesn't support
+      # this, only proxying existing methods.
+      module MockPublish
+        attr_reader :data, :opts, :called
+
+        def publish(data, opts)
+          @data = data
+          @opts = opts
+          @called = true
+        end
+      end
+
+      it 'should work and handle acks' do
+        mock(channel).acknowledge(37, false)
+
+        worker.do_work(@header, @props, :ack, @handler)
+      end
+
+      describe 'rejects' do
+        describe 'more retries ahead' do
+          it 'should work and handle rejects' do
+            mock(@header).routing_key { '#' }
+            mock(channel).acknowledge(37, false)
+
+            @backoff_exchange.extend MockPublish
+            worker.do_work(@header, @props_with_x_death, :reject, @handler)
+          end
+        end
+
+        describe 'no more retries' do
+          let(:max_retries) { 3 }
+
+          it 'sends the rejection to the error queue' do
+            mock(@header).routing_key { '#' }
+            mock(channel).acknowledge(37, false)
+
+            @error_exchange.extend MockPublish
+            @backoff_exchange.extend MockPublish
+            worker.do_work(@header, props_with_x_death_count, :reject, @handler)
+            @error_exchange.called.must_equal(true)
+            @error_exchange.opts.must_equal({ :routing_key => '#' })
+            data = JSON.parse(@error_exchange.data)
+            data['error'].must_equal('reject')
+            data['num_attempts'].must_equal(4)
+            data['payload'].must_equal(Base64.encode64(:reject.to_s))
+            data['properties'].must_equal(Base64.encode64(props_with_x_death_count.to_json))
+            Time.parse(data['failed_at']).wont_be_nil
+          end
+
+        end
+      end
+
+      describe 'requeues' do
+        it 'should work and handle requeues' do
+          mock(channel).reject(37, true)
+
+          worker.do_work(@header, @props_with_x_death, :requeue, @handler)
+        end
+
+        describe 'no more retries left' do
+          let(:max_retries) { 3 }
+
+          it 'continues to reject with requeue' do
+            mock(channel).reject(37, true)
+
+            worker.do_work(@header, @props_with_x_death, :requeue, @handler)
+          end
+        end
+
+      end
+
+      describe 'timeouts' do
+        describe 'more retries ahead' do
+          it 'should reject the message' do
+            mock(@header).routing_key { '#' }
+            mock(channel).acknowledge(37, false)
+
+            @backoff_exchange.extend MockPublish
+            worker.do_work(@header, @props_with_x_death, :timeout, @handler)
+          end
+        end
+
+        describe 'no more retries left' do
+          let(:max_retries) { 3 }
+
+          it 'sends the rejection to the error queue' do
+            mock(@header).routing_key { '#' }
+            mock(channel).acknowledge(37, false)
+            @error_exchange.extend MockPublish
+            @backoff_exchange.extend MockPublish
+
+            worker.do_work(@header, props_with_x_death_count, :timeout, @handler)
+            @error_exchange.called.must_equal(true)
+            @error_exchange.opts.must_equal({ :routing_key => '#' })
+            data = JSON.parse(@error_exchange.data)
+            data['error'].must_equal('timeout')
+            data['num_attempts'].must_equal(4)
+            data['payload'].must_equal(Base64.encode64(:timeout.to_s))
+            data['properties'].must_equal(Base64.encode64(props_with_x_death_count.to_json))
+            Time.parse(data['failed_at']).wont_be_nil
+          end
+        end
+      end
+
+      describe 'exceptions' do
+        describe 'more retries ahead' do
+          it 'should reject the message' do
+            mock(@header).routing_key { '#' }
+            mock(channel).acknowledge(37, false)
+            @backoff_exchange.extend MockPublish
+
+            worker.do_work(@header, @props_with_x_death, StandardError.new('boom!'), @handler)
+          end
+        end
+
+        describe 'no more retries left' do
+          let(:max_retries) { 3 }
+
+          it 'sends the rejection to the error queue' do
+            mock(@header).routing_key { '#' }
+            mock(channel).acknowledge(37, false)
+            @error_exchange.extend MockPublish
+            @backoff_exchange.extend MockPublish
+
+            worker.do_work(@header, props_with_x_death_count, StandardError.new('boom!'), @handler)
+            @error_exchange.called.must_equal(true)
+            @error_exchange.opts.must_equal({ :routing_key => '#' })
+            data = JSON.parse(@error_exchange.data)
+            data['error'].must_equal('boom!')
+            data['error_class'].must_equal(StandardError.to_s)
+            data['backtrace'].wont_be_nil
+            data['num_attempts'].must_equal(4)
+            data['payload'].must_equal(Base64.encode64('boom!'))
+            data['properties'].must_equal(Base64.encode64(props_with_x_death_count.to_json))
+            Time.parse(data['failed_at']).wont_be_nil
+          end
+        end
+      end
+
+      it 'should work and handle user-land error' do
+        mock(@header).routing_key { '#' }
+        mock(channel).acknowledge(37, false)
+        @backoff_exchange.extend MockPublish
+
+        worker.do_work(@header, @props, StandardError.new('boom!'), @handler)
+      end
+
+      it 'should work and handle noops' do
+        @backoff_exchange.extend MockPublish
+        worker.do_work(@header, @props, :wait, @handler)
+      end
+
+      # Since we encode in json, we want to make sure if the actual payload is
+      # json, then it's something you can get back out.
+      describe 'JSON payloads' do
+        let(:max_retries) { 3 }
+
+        it 'properly encodes the json payload' do
+          mock(@header).routing_key { '#' }
+          mock(channel).acknowledge(37, false)
+          @error_exchange.extend MockPublish
+
+          payload = {
+            data: 'hello',
+            response: :timeout
+          }
+          worker.do_work(@header, props_with_x_death_count, payload.to_json, @handler)
+          @error_exchange.called.must_equal(true)
+          @error_exchange.opts.must_equal({ :routing_key => '#' })
+          data = JSON.parse(@error_exchange.data)
+          data['error'].must_equal('timeout')
+          data['num_attempts'].must_equal(4)
+          data['payload'].must_equal(Base64.encode64(payload.to_json))
+          data['properties'].must_equal(Base64.encode64(props_with_x_death_count.to_json))
         end
 
       end
