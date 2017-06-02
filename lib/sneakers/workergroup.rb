a6f257fdd9912108da0e4ac5e6b5a02cd1e25cc1
Mike Bobrov <mike.bobrov@gmail.com>
2017-06-02 12:28:21 +0300

    fixed requested changes

--- lib/sneakers/workergroup.rb
+++ lib/sneakers/workergroup.rb
@@ -29,7 +29,7 @@ module Sneakers
         worker_classes = worker_classes.call
       end
 
-      bunny_connection = config[:share_bunny_connection] ? create_bunny_connection : nil
+      bunny_connection = create_connection_or_nil
 
       @workers = worker_classes.map{|w| w.new(nil, pool, {connection: bunny_connection}) }
       # if more than one worker this should be per worker
@@ -55,10 +55,12 @@ module Sneakers
     end
 
     def create_bunny_connection
-
       Bunny.new(Sneakers::CONFIG[:amqp], :vhost => Sneakers::CONFIG[:vhost], :heartbeat => Sneakers::CONFIG[:heartbeat], :logger => Sneakers::logger)
     end
-    private :create_bunny_connection
 
+    def create_connection_or_nil
+      config[:share_bunny_connection] ? create_bunny_connection : nil
+    end
+    private :create_bunny_connection, :create_connection_or_nil
   end
 end
